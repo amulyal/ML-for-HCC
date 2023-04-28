10b7fc47f27ab2600d29962d45bf00869a5d91fa
amulyal <amulyal333@gmail.com>
2023-04-28 15:18:30 +0530

    cors error

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import pickle
 import pandas as pd
 import numpy as np
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class ScoringItem(BaseModel):
     VasInv: int
